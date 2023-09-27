8ce4e81b954b559c4b639ad0fd5f1018bf3a9f9d
terrystansustal <terrysustal@gmail.com>
2023-09-26 18:33:46 -0700

    lower case port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connection');
 
-const port = process.env.PORT || 8080;
+const port = process.env.port || 8080;
 const app = express();
 
 app
