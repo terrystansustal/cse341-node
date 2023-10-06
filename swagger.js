060e591513ab84df9a38a01746efd93be482a9cc
terrystansustal <terrysustal@gmail.com>
2023-10-05 20:38:43 -0700

    Changed http to https

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
         description: 'Contacts API',
     },
     host: 'https://cse341-8296.onrender.com/',
-    schemes: ['http'],
+    schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
